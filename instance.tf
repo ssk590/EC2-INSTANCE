912da09024cf7c441f340492d6e48095fb7ce6b9
ssk590 <srujanshamantula@hotmail.com>
2017-05-12 14:23:56 -0500

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+   region = "eu-west-2"
+   shared_credentials_file = "path/to/.aws/creds"
+   profile = "AWS PROFILE"
+}
+resource "aws_instance" "MyAWSResource" {
+   ami             = ""
+   instance_type   = "t2.micro"
+}
